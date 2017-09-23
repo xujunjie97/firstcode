e6e7f5c2f8df767d455b847bc7ae7ff846e2517a
xujunjie97 <386382165@qq.com>
2017-09-23 14:40:29 +0800

    helloworld
    java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+package ewdqwd;
+
+/**
+ * Created by xujunjie on 2017/9/23.
+ */
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("hellow world!");
+    }
+}
